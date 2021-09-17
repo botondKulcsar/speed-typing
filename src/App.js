e5cf241d382e683ec29034c09c2207a8eeaf9075
Botond Kulcsar <bkulcsar@gmx.com>
2021-09-17 14:12:50 +0300

    game worx, css missing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react'
 import './App.css';
 
 function App() {
+  const STARTING_TIME = 10;
+
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [text, setText] = useState('');
+  const [numWords, setNumWords] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const textRef = useRef(null);
+
+  const calculateWords = (text) => text.trim().split(' ').map(word => word).length;
+
+  const endGame = () => {
+    setIsRunning(false);
+    const wordsTyped = calculateWords(text);
+    setNumWords(wordsTyped)
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && isRunning) {
+      setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1)
+      }, 1000)
+    } else if (timeRemaining <= 0) {
+      endGame();
+    }
+    
+  }, [timeRemaining, isRunning]);
+
+  const startGame = () => {
+    setIsRunning(true);
+    setText('');
+    setTimeRemaining(STARTING_TIME)
+    setNumWords(0);
+    textRef.current.disabled = false;
+    textRef.current.focus();
+  }
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How fast do you type?</h1>
+      <textarea
+        ref = { textRef }
+        value = { text }
+        onChange = { handleChange }
+        disabled = { !isRunning }
+       />
+      <h3>{isRunning ? `Time remaining: ${ timeRemaining } seconds` : 'Check your typing speed!'}</h3>
+      <button
+        onClick = { () => startGame() }
+        disabled = { isRunning }
+      >Start</button>
+      { numWords ? <h2>Result: <br /> { numWords } words in {STARTING_TIME} seconds</h2> : '' }
     </div>
   );
 }
